ad5867e2b0f07d3469c54b4c51a8934f60b4992e
jacksonargo <jacksonargo@users.noreply.github.com>
2015-01-23 15:31:32 -0500

    Update puppet-bootstrapper.bash
    Added better logic to the case statement. Added support for environment variables and config file.

--- puppet-bootstrapper.bash
+++ puppet-bootstrapper.bash
@@ -3,69 +3,60 @@
 
 # This script will install puppet and configure it for a first time run
 
-puppetmaster=puppetmaster  # Puppet master cert
-puppetmasterip=0.0.0.0     # Puppet master IP
-environment=testing        # Puppet environment
+# Defaults for our variables
+mycert=my.example.cert           # Cert for this client
+puppetmaster=master.example.cert # Puppet master cert
+puppetmasterip=0.0.0.0           # Puppet master IP
+environment=testing              # Puppet environment
 
+# Set from configuration file
+[ -r "$1" ] && . "$1"
+
+# Set from the environment
+[ "$MYCERT" != '' ] && mycert=$MYCERT
+[ "$PMCERT" != '' ] && puppetmaster=$PMCERT
+[ "$PMADDR" != '' ] && puppetmasterip=$PMADDR
+[ "$ENVIRO" != '' ] && environment=$ENVIRO
+
+# Check usage
+[ "$1" == '--help' ] && echo -e "Usage: $0 [CONF]\nInstall puppet and configure it for a first time run." && exit 0
 
 # We'll only run if we are root
 if [ $(whoami) != 'root' ]; then
-    echo "This script must be run as root"
+    echo "This script must be run as root."
     exit 1
 fi
-
-# If puppet is isntalled then we bail
-if [ -x /usr/bin/puppet ]; then
-    echo "Doing nothing because puppet is already installed; bye bye."
-    exit 1
-fi
-
-# Our operating system determines where we get the package
-uname -a | grep Ubuntu > /dev/null && os=deb
-uname -a | grep el7    > /dev/null && os=el7
-uname -a | grep el6    > /dev/null && os=el6
-uname -a | grep el5    > /dev/null && os=el5
-
-# Get the hostname of the machine
-hostname=$(hostname)
-
-echo
-echo "OS type: $operatingsystem"
-echo "Hostname: $hostname"
-echo
-echo
 
 # Function to quit the script
 quit () {
-    echo "An error has occured; we're bailing"
+    echo "An error has occured; we're bailing."
     exit 1
 }
 
 trap quit SIGINT SIGTERM
 
-# Check that /application_downloads exitst then move to it
-if [ -d /application_downloads ]; then
-    echo "Directory /application_downloads exists."
-else
-    echo "Directory /application_downloads does not exists; creating it."
-    mkdir /application_downloads
+# If puppet is installed then we bail
+if [ -x /usr/bin/puppet ]; then
+    echo "Doing nothing because puppet is already installed; bye bye."
+    exit 1
 fi
 
-echo
-echo "Moving to /applcation_downloads"
-echo
-cd /application_downloads
+# Determine the package to install
+uname -a | grep precise > /dev/null && os=deb && pkg=puppetlabs-release-precise.deb
+uname -a | grep el7     > /dev/null && os=el  && pkg=puppetlabs-release-el-7.noarch.rpm
+uname -a | grep el6     > /dev/null && os=el  && pkg=puppetlabs-release-el-6.noarch.rpm
+uname -a | grep el5     > /dev/null && os=el  && pkg=puppetlabs-release-el-5.noarch.rpm
 
 # Now we download and install puppet
 case $os in
-    "deb")
+    'deb')
         echo "Downloading puppet repo..."
         echo
-        wget -q http://apt.puppetlabs.com/puppetlabs-release-precise.deb || quit
+        wget -q http://apt.puppetlabs.com/$pkg || quit
         sleep 1
         echo "Installing repo..."
         echo
-        dpkg -i puppetlabs-release-precise.deb || quit
+        dpkg -i $pkg || quit
         sleep 1
         echo "Updating apt-get..."
         echo
@@ -76,37 +67,16 @@ case $os in
         apt-get -qq -y -o DPkg::Options::=--force-confold install puppet || quit
         sleep 1
         ;;
-    'el5')
+    'el')
         echo "Installing puppet repo..."
         echo
-        rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-5.noarch.rpm
+        rpm -ivh http://yum.puppetlabs.com/$pkg
         sleep 1
         echo "Installing puppet..."
         echo
         yum install puppet
         sleep 1
         ;;
-    'el6')
-        echo "Installing puppet repo..."
-        echo
-        rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
-        sleep 1
-        echo "Installing puppet..."
-        echo
-        yum install puppet
-        sleep 1
-        ;;
-    'el7')
-        echo "Installing puppet repo..."
-        echo
-        rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
-        sleep 1
-        echo "Installing puppet..."
-        echo
-        yum install puppet
-        sleep 1
-        ;;
-
     *) 
         echo "Sorry, your OS is not supported yet. Bye Bye."
         exit 1
@@ -127,7 +97,8 @@ echo "    report = true"              >> /etc/puppet/puppet.conf
 echo "    reports = log,store"        >> /etc/puppet/puppet.conf
 echo "[agent]"                        >> /etc/puppet/puppet.conf
 echo "    report = true"              >> /etc/puppet/puppet.conf
-echo "    server = $puppetmaster"  >> /etc/puppet/puppet.conf
+echo "    certname = $mycert"         >> /etc/puppet/puppet.conf
+echo "    server = $puppetmaster"     >> /etc/puppet/puppet.conf
 echo "    environment = $environment" >> /etc/puppet/puppet.conf
 
 echo
@@ -135,5 +106,7 @@ echo "Puppet is installed and ready for first run!"
 echo "Run 'puppet agent --test' to configure your system"
 echo
 
-echo "$puppetmasterip $puppetmaster" >> /etc/hosts
+# Add the puppet master to /etc/hosts
+grep $puppetmaster /etc/hosts > /dev/null || echo "$puppetmasterip $puppetmaster" >> /etc/hosts
+
 exit 0
